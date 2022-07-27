6a8e7bbdb7e45052b9708877e52b4e86ac7080e1
Iramis Valentin <85186645+git-ival@users.noreply.github.com>
2022-07-27 14:24:30 -0400

    revert

--- main.tf
+++ main.tf
@@ -294,10 +294,7 @@ resource "aws_autoscaling_attachment" "asg_attachment_bar" {
 resource "null_resource" "wait_for_ingress" {
   provisioner "local-exec" {
     command = <<-EOT
-      timeout --preserve-status 8m bash -c -- 'until kubectl get job --all-namespaces | grep -q "helm-install-rke2-ingress-nginx" ; do
-        sleep 5
-      done
-      kubectl --kubeconfig <(echo $KUBECONFIG | base64 --decode) -n kube-system wait --for=condition=complete job/helm-install-rke2-ingress-nginx'
+      timeout --preserve-status 5m bash -c -- 'kubectl --kubeconfig <(echo $KUBECONFIG | base64 --decode) -n kube-system wait --for=condition=complete job/helm-install-rke2-ingress-nginx'
     EOT
     environment = {
       KUBECONFIG = base64encode(data.aws_s3_object.kube_config.body)
